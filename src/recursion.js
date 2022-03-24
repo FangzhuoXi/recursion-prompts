5243db9880e1438faa9edccf6231a57cbd32ff18
Fangzhuo Xi <fangzhuoxi93@gmail.com>
2022-03-24 14:39:24 -0400

    13-18

--- src/recursion.js
+++ src/recursion.js
@@ -196,7 +196,46 @@ var multiply = function(x, y) {
 // 13. Write a function that divides two numbers without using the / operator or
 // Math methods to arrive at an approximate quotient (ignore decimal endings).
 var divide = function(x, y) {
+  if (y === 0) {
+    return NaN;
+  }
+  if (x === 0) {
+    return 0;
+  }
 
+  if (x > 0 && y > 0) {
+    if (x > y) {
+      return 1 + divide(x - y, y);
+    } else if (x < y) {
+      return 0
+    } else {
+      return 1;
+    }
+  } else if (x > 0 && y < 0) {
+    if (x + y > 0) {
+      return divide(x + y, y) - 1
+    } else if (x + y < 0) {
+      return 0;
+    } else {
+      return -1;
+    }
+  } else if (x < 0 && y > 0) {
+    if (x + y < 0) {
+      return divide(x + y, y) - 1
+    } else if (x + y > 0) {
+      return 0;
+    } else {
+      return -1;
+    }
+  } else if (x < 0 && y < 0) {
+    if (x < y) {
+      return 1 + divide(x - y, y);
+    } else if (x > y) {
+      return 0;
+    } else {
+      return 1;
+    }
+  }
 };
 
 // 14. Find the greatest common divisor (gcd) of two positive numbers. The GCD of two
@@ -205,6 +244,24 @@ var divide = function(x, y) {
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
 var gcd = function(x, y) {
+  if (x < 0 || y < 0) {
+    return null;
+  }
+  if (x === 0) {
+    return y;
+  } else if (y === 0) {
+    return x;
+  }
+
+  if (x > y) {
+    var remain = x % y;
+    return gcd(y, remain);
+  } else if (x < y) {
+    var remain = y % x;
+    return gcd(remain, x);
+  } else {
+    return x;
+  }
 };
 
 // 15. Write a function that compares each character of two strings and returns true if
@@ -212,21 +269,41 @@ var gcd = function(x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
+  if (str1.length === 0 && str2.length === 0) {
+    return true;
+  }
+  if (str1[0] === str2[0]) {
+    return compareStr(str1.slice(1), str2.slice(1));
+  } else {
+    return false;
+  }
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
 // occupies an index of the array.
 var createArray = function(str) {
+  if (str.length === 0) {
+    return [];
+  }
+  return [str[0]].concat(createArray(str.slice(1)));
 };
 
 // 17. Reverse the order of an array
 var reverseArr = function(array) {
+  if (array.length === 0) {
+    return [];
+  }
+  return [array[array.length - 1]].concat(reverseArr(array.slice(0, array.length - 1)));
 };
 
 // 18. Create a new array with a given value and length.
 // buildList(0,5) // [0,0,0,0,0]
 // buildList(7,3) // [7,7,7]
 var buildList = function(value, length) {
+  if (length === 0) {
+    return [];
+  }
+  return [value].concat(buildList(value, length - 1));
 };
 
 // 19. Implement FizzBuzz. Given integer n, return an array of the string representations of 1 to n.
